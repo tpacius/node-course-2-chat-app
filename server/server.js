fceb8f780ba8cbf58a83b68ac31fd9ac3ae282d1
Timothy Pacius <tpacius@gmail.com>
2019-01-17 15:52:34 -0500

    Added admin messages

--- server/server.js
+++ server/server.js
@@ -15,13 +15,31 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    //socket.emit from admin with text Welcome
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app!',
+        createdAt: new Date().getTime()
+    });
+    //socket.broadcast.emit from admit New user joined
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New used joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
         io.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
-        })
+        });
+        // socket.broadcast.emit('newMessage', {
+        //         from: message.from,
+        //         text: message.text,
+        //         createdAt: new Date().getTime()
+        //     });
     });
 
     socket.on('disconnect', () => {
