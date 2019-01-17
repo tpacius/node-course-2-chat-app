f9ec8cb8de4c32354a23bd5b7cb2fc06c4c5bf20
Timothy Pacius <tpacius@gmail.com>
2019-01-17 15:29:20 -0500

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('Connected to server');
-    
-    socket.emit('createMessage', {
-        from: 'Drew',
-        text: 'Not much bro'
-    });
 });
 
 socket.on('disconnect', function() {
@@ -14,5 +9,5 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(message) {
-    console.log('New chat', message);
+    console.log('New Message', message);
 });
--- server/server.js
+++ server/server.js
@@ -15,14 +15,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        to: 'Tim',
-        text: 'Hey what up',
-        createdAt: Date().toString()
-    });
-
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
     });
 
     socket.on('disconnect', () => {
