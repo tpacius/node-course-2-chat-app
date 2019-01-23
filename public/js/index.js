e77c2b97316456be468f92c570e6b48b2ae7f1a1
Timothy Pacius <tpacius@gmail.com>
2019-01-22 20:28:24 -0500

    Updated styles

--- public/js/index.js
+++ public/js/index.js
@@ -16,13 +16,15 @@ socket.on('newMessage', function(message) {
     jQuery('#messages').append(li);
 });
 
+var messageText = jQuery('[name=message]');
+
 jQuery('#message-form').on('submit', function(e) {
     e.preventDefault();
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageText.val()
     }, function() {
-
+        messageText.val('');
     });
 });
 
@@ -43,13 +45,16 @@ locationButton.on('click', function() {
         return alert('Geolocation not supported by your browser.');
     }
 
-    navigator.geolocation.getCurrentPosition(function(position) {
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
 
+    navigator.geolocation.getCurrentPosition(function(position) {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function() {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location');
     })
 });
