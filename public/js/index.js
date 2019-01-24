d88c9001cbbc4e3e6595e2939c7f4da879431686
Timothy Pacius <tpacius@gmail.com>
2019-01-23 21:11:08 -0500

    Added Mustache for messages

--- public/js/index.js
+++ public/js/index.js
@@ -10,11 +10,13 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
     var formattedTime = moment(message.createdAt).format('h:mm a');
-    console.log('New Message', message);
-    var li = jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
-
-    jQuery('#messages').append(li);
+    var template = jQuery('#message-template').html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
+    jQuery('#messages').append(html);
 });
 
 var messageText = jQuery('[name=message]');
@@ -30,14 +32,15 @@ jQuery('#message-form').on('submit', function(e) {
 });
 
 socket.on('newLocationMessage', function(message) {
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target=_blank>My current location</a>')
     var formattedTime = moment(message.createdAt).format('h:mm a');
-
-    li.text(`${message.from} ${formattedTime}: `);
-    a.attr('href', message.url);
-    li.append(a);
-    jQuery('#messages').append(li);
+    var template = jQuery('#location-message-template').html();
+    var html = Mustache.render(template, {
+        from: message.from,
+        url: message.url,
+        createdAt: formattedTime
+    });
+    
+    jQuery('#messages').append(html);
 });
 
 var locationButton = jQuery('#send-location');
