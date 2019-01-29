ab778ec756201f4658eca3746ecb3643420f9a8e
Timothy Pacius <tpacius@gmail.com>
2019-01-28 19:01:42 -0500

    Added autoscroll logic

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom() {
+    //Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    //Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function() {
     console.log('Connected to server');
 });
@@ -17,6 +33,7 @@ socket.on('newMessage', function(message) {
         createdAt: formattedTime
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 var messageText = jQuery('[name=message]');
@@ -41,6 +58,7 @@ socket.on('newLocationMessage', function(message) {
     });
     
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 var locationButton = jQuery('#send-location');
